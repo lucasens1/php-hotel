c8db4dbe8cf5ac6c4c4d529688145fab45561a94
lucasens1 <lucasensini1@gmail.com>
2024-05-21 15:22:50 +0200

    print on page ok

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Hotel</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <section class="ms_container">
+        <?php foreach($hotels as $cur_hotels) { ?>
+        <div class="ms_card">
+            <h4><?php echo $cur_hotels['name'] ?></h4>
+            <p><?php echo $cur_hotels['description'] ?></p>
+            <p><?php echo $cur_hotels['vote'] ?></p>
+            <p><?php echo $cur_hotels['distance_to_center'] ?></p>
+        </div>
+        <?php } ?>
+    </section>
+</body>
+</html>
